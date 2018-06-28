b59da4978ae07ff7c9ae608ef27a2ba1cf054701
LeeQang <545540710@qq.com>
2018-06-28 11:07:35 +0800

    添加手机适配的版本

---
+++ web-mobile/src/assets/Script/define/define.c524b.js
@@ -0,0 +1,1 @@
+window.TIME=5,window.DEBUG=!0,window.STATUS=cc.Enum({START:-1,RIGHT:-1,WRONG:-1,HOME:-1,PAUSE:-1}),window.STATUS_NAME=["START","RIGHT","WRONG","HOME","PAUSE"],window.RANK=cc.Enum({QUN:-1,FRIEND:-1,END:-1}),window.VARY=.01,window.SUBVIEW_RANK=1,window.SUBVIEW_END=2,window.gloablUserIfo={openid:"0",session_key:"0",url:"",name:""};
